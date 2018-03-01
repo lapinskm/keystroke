9de36fba71afa2402647c8c214f9f19df941e5d9
Marcin Łapiński <lapinskm@gmail.com>
2018-03-01 21:27:34 +0100

    Added keyboard input logging.

---
+++ keyboardListener.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import sys
+from PyQt4.QtGui import *
+
+
+def on_key(key):
+   print "### KEY text: " + key.text()
+   print  key.key()
+
+def on_keydown(key):
+   print "############# key pressed  ###";
+   on_key(key)
+
+def on_keyup(key):
+   print "############# key released ###";
+   on_key(key)
+
+
+# Create an PyQT4 application object.
+a = QApplication(sys.argv)
+
+# The QWidget widget is the base class of all user interface objects in PyQt4.
+w = QWidget()
+
+
+# Set window size.
+w.resize(320, 240)
+
+
+w.setWindowTitle("key stroke recorder")
+w.keyPressEvent=on_keydown
+w.keyReleaseEvent=on_keyup
+
+# Show window
+w.show()
+
+sys.exit(a.exec_())
