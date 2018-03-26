27e0976b415c17a362cc8062fde33c75a1419b55
Marcin Łapiński <lapinskm@gmail.com>
2018-03-26 19:28:10 +0200

    Added Main class.
     To encapsulate all program data.

--- keyboardListener.py
+++ keyboardListener.py
@@ -7,34 +7,37 @@ from PyQt4.QtCore import QDateTime
 
 import recorderUi
 
-def on_key(key):
-   print "### KEY text: " + key.text()
-   print  key.key()
-   print  QDateTime.currentMSecsSinceEpoch()
+class Main:
 
-def on_keydown(key):
-   print "############# key pressed  ###";
-   on_key(key)
+   def on_key(self, key):
+      print "### KEY text: " + key.text()
+      print  key.key()
+      print  QDateTime.currentMSecsSinceEpoch()
 
-def on_keyup(key):
-   print "############# key released ###";
-   on_key(key)
+   def on_keydown(self, key):
+      print "############# key pressed  ###";
+      self.on_key(key)
 
+   def on_keyup(self, key):
+      print "############# key released ###";
+      self.on_key(key)
 
-# Create an PyQT4 application object.
-a = QApplication(sys.argv)
+   def __init__(self):
+      # Create an PyQT4 application object.
+     self.app = QApplication(sys.argv)
 
-# The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QWidget()
-# Create ui
-ui = recorderUi.Ui_MainWindow()
-ui.setupUi(w)
+     # The QWidget widget is the base class of all user interface objects in PyQt4.
+     self.widget = QWidget()
+     # Create ui
+     self.ui = recorderUi.Ui_MainWindow()
+     self.ui.setupUi(self.widget)
 
+     self.widget.keyPressEvent   = self.on_keydown
+     self.widget.keyReleaseEvent = self.on_keyup
+   
+   def run(self):
+     # Show window
+     self.widget.show()
+     sys.exit(self.app.exec_())
 
-w.keyPressEvent=on_keydown
-w.keyReleaseEvent=on_keyup
-
-# Show window
-w.show()
-
-sys.exit(a.exec_())
+Main().run();
